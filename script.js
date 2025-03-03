f72867f479108db39f9ba15a0ecee771139c737f
AzureAD\JunWeiKhor <khorjunweiwei@gmail.com>
2025-03-03 16:40:06 +0800

    add files

---
+++ script.js
@@ -0,0 +1,64 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 400;
+canvas.height = 400;
+
+const box = 20;
+let snake = [{ x: 200, y: 200 }];
+let direction = "RIGHT";
+let food = {
+    x: Math.floor(Math.random() * (canvas.width / box)) * box,
+    y: Math.floor(Math.random() * (canvas.height / box)) * box
+};
+
+document.addEventListener("keydown", changeDirection);
+
+function changeDirection(event) {
+    const key = event.key;
+    if (key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+    if (key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+    if (key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+    if (key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+}
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, box, box);
+
+    ctx.fillStyle = "lime";
+    snake.forEach((part, index) => {
+        ctx.fillRect(part.x, part.y, box, box);
+    });
+
+    let newHead = { ...snake[0] };
+    if (direction === "UP") newHead.y -= box;
+    if (direction === "DOWN") newHead.y += box;
+    if (direction === "LEFT") newHead.x -= box;
+    if (direction === "RIGHT") newHead.x += box;
+
+    if (newHead.x === food.x && newHead.y === food.y) {
+        food = {
+            x: Math.floor(Math.random() * (canvas.width / box)) * box,
+            y: Math.floor(Math.random() * (canvas.height / box)) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+    if (
+        newHead.x < 0 || newHead.x >= canvas.width ||
+        newHead.y < 0 || newHead.y >= canvas.height ||
+        snake.some(part => part.x === newHead.x && part.y === newHead.y)
+    ) {
+        alert("Game Over");
+        location.reload();
+    }
+
+    snake.unshift(newHead);
+}
+
+setInterval(draw, 100);
